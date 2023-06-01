e8d5de5159dc7c7076f3e64d4a91b6ccacc3b3ef
ariwiyogiarto <106458532+ariwiyogiarto@users.noreply.github.com>
2023-06-01 14:28:00 +0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+nilai1=int(input("masukkan nilai 1 :"))
+nilai2=int(input("masukkan nilai 2 :"))
+operasi=input("+-*/%")
+if operasi=='+' :
+	print(nilai1+nilai2)
+elif operasi=='-':
+	print(nilai1-nilai2) 
+elif operasi=='*' :
+	print(nilai1*nilai2)
+elif operasi=='/' :
+	print(nilai1/nilai2)
+elif operasi=='% ':
+	print(nilai1%nilai2)
